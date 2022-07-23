9023e00dfc7279499564d02b64c9d57357d51579
Nweke Emmanuel <79476502+Manuel-co@users.noreply.github.com>
2022-07-23 07:37:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,23 +11,18 @@ const cover = document.getElementById('cover');
 const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
-// Song titles
 const songs = ['summertime','moonlight-mastered','coding-night', 'summer','tech-house-vibes', 'hey', 'leonell-cassio-chapter-two-ft-carrie'];
 
-// Keep track of song
 let songIndex = 2;
 
-// Initially load song details into DOM
 loadSong(songs[songIndex]);
 
-// Update song details
 function loadSong(song) {
   title.innerText = song;
   audio.src = `music/${song}.mp3`;
   cover.src = `images/${song}.jpg`;
 }
 
-// Play song
 function playSong() {
   musicContainer.classList.add('play');
   playBtn.querySelector('i.fas').classList.remove('fa-play');
@@ -36,7 +31,6 @@ function playSong() {
   audio.play();
 }
 
-// Pause song
 function pauseSong() {
   musicContainer.classList.remove('play');
   playBtn.querySelector('i.fas').classList.add('fa-play');
@@ -45,7 +39,6 @@ function pauseSong() {
   audio.pause();
 }
 
-// Previous song
 function prevSong() {
   songIndex--;
 
@@ -58,7 +51,6 @@ function prevSong() {
   playSong();
 }
 
-// Next song
 function nextSong() {
   songIndex++;
 
@@ -71,14 +63,13 @@ function nextSong() {
   playSong();
 }
 
-// Update progress bar
 function updateProgress(e) {
   const { duration, currentTime } = e.srcElement;
   const progressPercent = (currentTime / duration) * 100;
   progress.style.width = `${progressPercent}%`;
 }
 
-// Set progress bar
+
 function setProgress(e) {
   const width = this.clientWidth;
   const clickX = e.offsetX;
@@ -87,18 +78,15 @@ function setProgress(e) {
   audio.currentTime = (clickX / width) * duration;
 }
 
-//get duration & currentTime for Time of song
 function DurTime (e) {
 	const {duration,currentTime} = e.srcElement;
 	var sec;
 	var sec_d;
 
-	// define minutes currentTime
 	let min = (currentTime==null)? 0:
 	 Math.floor(currentTime/60);
 	 min = min <10 ? '0'+min:min;
 
-	// define seconds currentTime
 	function get_sec (x) {
 		if(Math.floor(x) >= 60){
 			
@@ -116,10 +104,8 @@ function DurTime (e) {
 
 	get_sec (currentTime,sec);
 
-	// change currentTime DOM
 	currTime.innerHTML = min +':'+ sec;
 
-	// define minutes duration
 	let min_d = (isNaN(duration) === true)? '0':
 		Math.floor(duration/60);
 	 min_d = min_d <10 ? '0'+min_d:min_d;
@@ -141,16 +127,13 @@ function DurTime (e) {
 		 }
 	} 
 
-	// define seconds duration
 	
 	get_sec_d (duration);
 
-	// change duration DOM
 	durTime.innerHTML = min_d +':'+ sec_d;
 		
 };
 
-// Event listeners
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
 
@@ -161,18 +144,13 @@ playBtn.addEventListener('click', () => {
   }
 });
 
-// Change song
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
-// Time/song update
 audio.addEventListener('timeupdate', updateProgress);
 
-// Click on progress bar
 progressContainer.addEventListener('click', setProgress);
 
-// Song ends
 audio.addEventListener('ended', nextSong);
 
-// Time of song
 audio.addEventListener('timeupdate',DurTime);
